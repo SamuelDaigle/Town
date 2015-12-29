b824c843f62cae7985d23cc96b983c8ca4665860
Samuel Daigle <samuel_daigle@outlook.com>
2015-12-28 20:29:46 -0500

    Added pathfinding.

--- Assets/Scripts/VillagerAI/CharacterBehavior.cs
+++ Assets/Scripts/VillagerAI/CharacterBehavior.cs
@@ -1,53 +1,68 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
+using System.Collections.Generic;
 
 namespace VillagerAI
 {
-	public class CharacterBehavior : MonoBehaviour
-	{
-        public Text debugText;
+    public class CharacterBehavior : MonoBehaviour
+    {
+        public GameObject debugText;
+
+        private TextMesh textMesh;
         private Character character;
-	    private Transform target;
-	
-	    // Use this for initialization
-	    void Start()
-	    {
+        private Transform target;
+        private Pathfinding pathfinding;
+
+        // Use this for initialization
+        void Start()
+        {
             character = new Character(this);
-	    }
-	
-	    // Update is called once per frame
-	    void Update()
-	    {
+            textMesh = debugText.GetComponent<TextMesh>();
+            pathfinding = GetComponent<Pathfinding>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
             character.Update();
 
             ShowDebug();
-	    }
+        }
 
         private void ShowDebug()
         {
-            Debug.Assert(debugText != null);
-            debugText.transform.position = transform.position + Vector3.up * transform.localScale.y;
+            Debug.Assert(textMesh != null);
             DebugText text = character.GetDebugText();
-            debugText.text = text.ClassText + "\n" + text.StateText + "\n" + text.ResourceCountText;
+            textMesh.text = text.ClassText + "\n" + text.StateText + "\n" + text.ResourceCountText;
         }
 
         public void MoveToTarget()
         {
             Debug.Assert(target != null);
-            Vector3 direction = (target.position - transform.position).normalized;
-            transform.Translate(direction * Time.deltaTime);
+            /*Vector3 direction = (target.position - transform.position).normalized;
+            transform.Translate(direction * Time.deltaTime);*/
+            pathfinding.FindPath(transform.position, target.position);
+            pathfinding.Move();
         }
 
         public Transform GetTransform()
         {
             return transform;
         }
-	
-	    public void SetTarget(string _tag)
-	    {
-	        target = AISight.GetObjects(transform, _tag)[0].transform;
-	    }
+
+        public void SetTarget(string _tag)
+        {
+            List<GameObject> sortedTargets = AISight.GetObjects(transform, _tag);
+            if (sortedTargets.Count == 0)
+            {
+                target = transform;
+            }
+            else
+            {
+                target = sortedTargets[0].transform;
+            }
+        }
 
         public Transform GetTarget()
         {
@@ -69,5 +84,5 @@ namespace VillagerAI
         {
             gameObject.GetComponent<MeshRenderer>().material = Resources.Load(_imagePath) as Material;
         }
-	}
+    }
 }
