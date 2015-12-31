08ebe3f6ac5f6eb3a2fe317a1a3518bc9a66d86e
Samuel Daigle <samuel_daigle@outlook.com>
2015-12-30 21:16:23 -0500

    First visible mountains.

--- Assets/Scripts/Level/TerrainLoading.cs
+++ Assets/Scripts/Level/TerrainLoading.cs
@@ -12,6 +12,8 @@ public class TerrainLoading : MonoBehaviour
     private int sizeX;
     private int sizeY;
 
+    private const int MAX_RECURSIVE_LEVEL = 7;
+
     void Start()
     {
         terrain = Terrain.activeTerrain;
@@ -45,13 +47,14 @@ public class TerrainLoading : MonoBehaviour
         SetEmptyHeight(0, sizeY, 0.02f);
         SetEmptyHeight(sizeX, 0, 0.02f);
         SetEmptyHeight(sizeX, sizeY, 0.02f);
-        DiamondSquare(0, 0, sizeX, sizeY, 0.1f);
+        DiamondSquare(0, 0, sizeX, sizeY, 1f, MAX_RECURSIVE_LEVEL);
         Debug.Log("Done.");
     }
 
-    private void DiamondSquare(int _left, int _top, int _right, int _bottom, float base_height)
+    private void DiamondSquare(int _left, int _top, int _right, int _bottom, float base_height, int _recursiveLevel)
     {
-        Debug.Log("A mountain as been created.");
+        if (_recursiveLevel == 0) return;
+
         // Set the Center point of the Diamond step.
         int xCenter = (int)Mathf.Floor(_left + _right) / 2;
         int yCenter = (int)Mathf.Floor(_top + _bottom) / 2;
@@ -59,20 +62,16 @@ public class TerrainLoading : MonoBehaviour
         SetEmptyHeight(xCenter, yCenter, centerPointHeight);
 
         // Set the four corners of the Square step.
-        SetEmptyHeight(xCenter, _top, GetHeight(_left, _top) + GetHeight(_right, _top) / 2 + (Random.value - 0.5f) * base_height);
-        SetEmptyHeight(xCenter, _bottom, GetHeight(_left, _bottom) + GetHeight(_right, _bottom) / 2 + (Random.value - 0.5f) * base_height);
-        SetEmptyHeight(_left, yCenter, GetHeight(_left, _top) + GetHeight(_left, _bottom) / 2 + (Random.value - 0.5f) * base_height);
-        SetEmptyHeight(_right, yCenter, GetHeight(_right, _top) + GetHeight(_right, _bottom) / 2 + (Random.value - 0.5f) * base_height);
+        SetEmptyHeight(xCenter, _top, (GetHeight(_left, _top) + GetHeight(_right, _top)) / 2);
+        SetEmptyHeight(xCenter, _bottom, (GetHeight(_left, _bottom) + GetHeight(_right, _bottom)) / 2);
+        SetEmptyHeight(_left, yCenter, (GetHeight(_left, _top) + GetHeight(_left, _bottom)) / 2);
+        SetEmptyHeight(_right, yCenter, (GetHeight(_right, _top) + GetHeight(_right, _bottom)) / 2);
 
         // Call smaller regions.
-        if ((_right - _left) > 16)
-        {
-            base_height = base_height * Mathf.Pow(2f, -0.75f);
-            DiamondSquare(_left, _top, xCenter, yCenter, base_height);
-            DiamondSquare(xCenter, _top, _right, yCenter, base_height);
-            DiamondSquare(_left, yCenter, xCenter, _bottom, base_height);
-            DiamondSquare(xCenter, yCenter, _right, _bottom, base_height);
-        }
+        DiamondSquare(_left, _top, xCenter, yCenter, base_height, _recursiveLevel - 1);
+        DiamondSquare(xCenter, _top, _right, yCenter, base_height, _recursiveLevel - 1);
+        DiamondSquare(_left, yCenter, xCenter, _bottom, base_height, _recursiveLevel - 1);
+        DiamondSquare(xCenter, yCenter, _right, _bottom, base_height, _recursiveLevel - 1);
     }
 
     private float[,] GetTerrainHeights()
@@ -94,7 +93,7 @@ public class TerrainLoading : MonoBehaviour
     {
         if (heights[_y, _x] == 0)
         {
-            heights[_y, _x] = _value / 3;
+            heights[_y, _x] = _value + Random.RandomRange(0, 0.001f);
         }
     }
 }
